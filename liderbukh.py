3d6240853e311b90fb84212a817288868b647127
Marc Trius <derpayatz@gmail.com>
2019-11-22 22:32:27 -0600

    Added capability to process single song

--- liderbukh.py
+++ liderbukh.py
@@ -55,9 +55,10 @@ class Liderbukh():
         book_data = []
         
         try:
-            songlist = [meta for meta in os.listdir( os.path.join(
+            songlist = [
+                os.path.splitext(metafile)[0] for metafile in os.listdir( os.path.join(
                     self.settings['root_dir'], 
-                    self.settings['data_dir'])) if meta.endswith(".yaml")]
+                    self.settings['data_dir'])) if metafile.endswith(".yaml")]
         except Exception as e:
             print("Could not read song meta files: %s" % (e))
             raise
@@ -65,88 +66,87 @@ class Liderbukh():
         for chapter_name in self.settings['book']['chapters']:
             book_data.append( {'name': chapter_name, 'songs': [] } )
         
-        for song in songlist:
-            filename = os.path.splitext(song)[0]
+        for filename in songlist:
+            song = self.process_song(filename)
+            try:
+                    book_data[int(song['meta']['chapter'])]['songs'].append(song)
+            except IndexError as e:
+                    print('Error processing chapter number %s for %s: %s' % (
+                        song['meta']['chapter'], song['meta']['filename'], e))
+                    raise
+        
+        return book_data
+      
+    def process_song(self, filename):
             base_path = os.path.join(self.settings['root_dir'],  # For output file paths
                     self.settings['temp_dir'], filename )
-            cur_song = {
+            song = {
                 'meta':{
                     'filename': filename,
                     'ly_path': ''.join([ base_path, '.ly'] ),
                     'tex_path': ''.join([ base_path, '.lytex'])
                     } }
             
-            #print( cur_song['meta']['filename'] )
             for field in self.settings['song_meta']:
-                cur_song['meta'].update({field: None})
+                song['meta'].update({field: None})
             
             try:
-                meta = yaml.load(self.load_file(song))
+                meta = yaml.load(self.load_file('%s.yaml' % filename ))
             except yaml.scanner.ScannerError as e:
                 print("Invalid song file: %s\nYAML error: %s" % (song, e) )
                 raise
             
             try:
                 for key in meta:
-                    cur_song['meta'][key] = meta[key]
+                    song['meta'][key] = meta[key]
             except KeyError as e:
                 print("Invalid song file: %s\nField %s not known." 
                       % (song, e) )
                 raise
-            #print(cur_song)
             
             # Load the music data
             try:
-                cur_song['music'] = self.load_file(
-                    ''.join([cur_song['meta']['filename'],'.ly'])).replace(
+                song['music'] = self.load_file(
+                    ''.join([song['meta']['filename'],'.ly'])).replace(
                             '\include "templates/preamble.ly"',
                             self.load_file(
                                     'preamble.ly', self.settings['templates_dir']))
             except Exception:
-                print( 'Error loading music data for %s' % ( cur_song['meta']['filename'] ) )
+                print( 'Error loading music data for %s' % ( song['meta']['filename'] ) )
                 raise
             
             # Overwrite with template output
             try:
-                cur_song['music'] = self.build_template(
-                        cur_song, 'lilypond.template' )
-            #print(cur_song['music'])
+                song['music'] = self.build_template(
+                        song, 'lilypond.template' )
             except Exception:
-                print( 'Error building Lilypond template for %s' % ( cur_song['meta']['filename'] ) )
+                print( 'Error building Lilypond template for %s' % ( song['meta']['filename'] ) )
                 raise
             
             # Load the lyrics data
             try:
-                cur_song['lyrics'] = self.load_file(''.join([cur_song['meta']['filename'],
+                song['lyrics'] = self.load_file(''.join([song['meta']['filename'],
                         self.settings['lyrics_ext']]))
             except Exception:
-                print( 'Error loading lyrics for %s' % ( cur_song['meta']['filename'] ) ) 
+                print( 'Error loading lyrics for %s' % ( song['meta']['filename'] ) ) 
                 raise
             
             # Render TeX
             try:
-                cur_song['tex'] = self.render_tex( cur_song['lyrics'] )
+                song['tex'] = self.render_tex( song['lyrics'] )
             except Exception:
-                print( 'Error rendering TeX for %s' % ( cur_song['meta']['filename'] ) )
+                print( 'Error rendering TeX for %s' % ( song['meta']['filename'] ) )
                 raise
             
             # Load TeX template output 
             try:
-                cur_song['tex'] = self.build_template( 
-                    cur_song, 'tex.template' )
+                song['tex'] = self.build_template( 
+                    song, 'tex.template' )
             except Exception:
-                print( 'Error building TeX template for %s' % ( cur_song['meta']['filename'] ) )
+                print( 'Error building TeX template for %s' % ( song['meta']['filename'] ) )
                 raise
             
-            #print(cur_song['tex'])
-            try:
-                book_data[int(meta['chapter'])]['songs'].append(cur_song)
-            except IndexError as e:
-                print('Error processing chapter number for %s: %s' % (
-                    cur_song['meta']['filename'], e))
-                raise
-        
-        return book_data
+            return song
     
     def build_template(self, data, template):
         
@@ -155,7 +155,6 @@ class Liderbukh():
                 template, self.settings['templates_dir'])
         template_args['data'] = data
         template_args['escape'] = None
-        #print('Rendering %s' % data['meta']['songname_eng'])
         try:
             return pyratemp.Template(**template_args)()
         except Exception:
@@ -164,7 +163,7 @@ class Liderbukh():
             raise
     
     # Write files
-    def write_files(self):
+    def write_files(self, song):
         
         output_dir = self.settings['output_dir']
         temp_dir = self.settings['temp_dir']
@@ -175,81 +174,103 @@ class Liderbukh():
         if not os.path.isdir(temp_dir):
             os.mkdir(temp_dir)
         
-        for chapter in self.book_data:
-            for song in chapter['songs']:
-                
-                tex_path = song['meta']['tex_path']
-                ly_path = song['meta']['ly_path']
-                pdf_path = ''.join([
-                    os.path.join(output_dir, song['meta']['filename']),
-                    '.pdf'])
-                    
-                print('Writing:\t\t%s' % tex_path)
-                try:
-                    with open(tex_path, 'w+', encoding='utf-8') as f:
-                        f.write(song['tex'])
-                        print('Success!\n')
-                except Exception as e:
-                    print( 'Error: %s\n' % (e) )
-                    
-                print('Writing:\t\t%s' % ly_path)
-                try:
-                    with open(ly_path, 'w+', encoding='utf-8') as f:
-                        f.write(song['music'])
-                        print('Success!\n')
-                except Exception as e:
-                    print( 'Error: %s\n' % (e) )
-                
-                filename = song['meta']['filename']
-                
-                print('Writing:\t\t%s:' % (pdf_path) )
-                print('Running lilypond-book')
+        tex_path = song['meta']['tex_path']
+        ly_path = song['meta']['ly_path']
+        pdf_path = ''.join([
+            os.path.join(output_dir, song['meta']['filename']),
+            '.pdf'])
+            
+        print('Writing:\t\t%s' % tex_path)
+        try:
+            with open(tex_path, 'w+', encoding='utf-8') as f:
+                f.write(song['tex'])
+                print('Success!\n')
+        except Exception as e:
+            print( 'Error: %s\n' % (e) )
+            
+        print('Writing:\t\t%s' % ly_path)
+        try:
+            with open(ly_path, 'w+', encoding='utf-8') as f:
+                f.write(song['music'])
+                print('Success!\n')
+        except Exception as e:
+            print( 'Error: %s\n' % (e) )
+        
+        filename = song['meta']['filename']
+        
+        print('Writing:\t\t%s:' % (pdf_path) )
+        print('Running lilypond-book')
 
-                try:
-                    subprocess.run([
-                        'lilypond-book',
-                        '--latex-program=xelatex',
-                        '--output=%s' % ( temp_dir ),
-                        '--loglevel=ERROR',
-                        song['meta']['tex_path']])
-                    print('Success!\n')
-                except Exception as e:
-                    print( 'Error: %s' % (e) )
-                
-                print( 'Running XeLaTeX' )
-                try:
-                    os.chdir( temp_dir )
-                    subprocess.run([
-                        'xelatex',
-                        '-interaction=batchmode',
-                        '-halt-on-error',
-                        '%s.tex' % ( filename )])
-                    os.chdir(root_dir)
-                    subprocess.run([
-                        'mv',
-                        '%s/%s.pdf' % (temp_dir, filename),
-                        '%s/%s.pdf' % (self.settings['output_dir'], filename)])
-                    print('Success!\n')
-                except Exception as e:
-                    print ( 'Error: %s' % (e) )
-                    raise
+        try:
+            subprocess.run([
+                'lilypond-book',
+                '--latex-program=xelatex',
+                '--output=%s' % ( temp_dir ),
+                '--loglevel=ERROR',
+                song['meta']['tex_path']])
+            print('Success!\n')
+        except Exception as e:
+            print( 'Error: %s' % (e) )
+        
+        print( 'Running XeLaTeX' )
+        try:
+            os.chdir( temp_dir )
+            subprocess.run([
+                'xelatex',
+                '-interaction=batchmode',
+                '-halt-on-error',
+                '%s.tex' % ( filename )])
+            os.chdir(root_dir)
+            subprocess.run([
+                'mv',
+                '%s/%s.pdf' % (temp_dir, filename),
+                '%s/%s.pdf' % (self.settings['output_dir'], filename)])
+            print('Success!\n')
+        except Exception as e:
+            print ( 'Error: %s' % (e) )
+            raise
     
 
 @click.command()
-@click.option('--settings-file', '-s', default='settings.yaml', help='Settings file. Default: settings.yaml', type=click.Path(exists=True))
-@click.option('--debug', '-d', default=False, is_flag=True, help='Turn debug mode on')
-@click.option('--no-write', '-n', default=False, is_flag=True, help='Generate song data but don\'t write any files')
 
-def main(settings_file, debug, no_write):
+@click.option('--settings-file',
+              '-s', default='settings.yaml',
+              help='Settings file. Default: settings.yaml',
+              type=click.Path(exists=True))
+
+@click.option(
+  '--debug', '-d', default=False, is_flag=True,
+  help='Turn debug mode on')
+
+@click.option('--no-write',
+              '-n',
+              default=False,
+              is_flag=True,
+              help='Generate song data but don\'t write any files')
+
+@click.option('--filename',
+              '-f',
+              default=None,
+              help='Only process this song file. \
+              Please provide filename without extension.')
+
+def main(settings_file, debug, no_write, filename):
     book = Liderbukh(settings_file, debug)
-    try:
-        book.book_data = book.build_book_data()
-    except Exception as e:
-        print ( "Failed to build book data" )
-        if debug: raise
-        sys.exit(1)
-    if not no_write:
-        book.write_files()
+    if filename:
+        data = book.process_song(filename)
+        if not no_write:
+                book.write_files(data)
+    else:
+        try:
+            data = book.build_book_data()
+        except Exception as e:
+            print ( "Failed to build book data" )
+            if debug: raise
+            sys.exit(1)
+        if not no_write:
+            for chapter in data:
+                for song in chapter['songs']:
+                    book.write_files(song)
 
 if __name__ == '__main__':
     main()
