a6cc2978b2fae434900a457efd14015f16c4af8a
Marc Trius <derpayatz@gmail.com>
2019-12-14 17:30:11 -0600

    split meta file reading into separate function and winnowed testing code

--- liderbukh2.py
+++ liderbukh2.py
@@ -24,20 +24,22 @@ import yaml
 import sys
 import timeit
 
-def list_dirs_meta( path ):
+def index_dirs( path ):
     for dir_path in (
             directory.path
             for directory in os.scandir( path ) 
             if directory.is_dir()
             and os.path.isfile(
                 os.path.join( directory.path, 'meta.yaml' ) ) ):
-        with open( os.path.join( dir_path, 'meta.yaml' ) ) as meta_file:
+        yield os.path.split(dir_path)[1], dir_path
+
+def read_dir_meta( path ):
+        with open( os.path.join( path, 'meta.yaml' ) ) as meta_file:
             try:
-                dir_meta = yaml.load( meta_file )
+                meta = yaml.load( meta_file )
+                return meta
             except Exception as e:
                 print('YAML error: %s' % e )
-                continue
-        yield os.path.split(dir_path)[1], dir_path, dir_meta
 
 def is_var(i): 
         if ( 'entry_slug' in i.keys() ): 
@@ -46,50 +48,25 @@ def is_var(i):
 
 def index(data_dir='data'):
     try:
-        for cat_slug, cat_path, cat_meta in list_dirs_meta( data_dir ):
-            for entry_slug, entry_path, entry_meta in list_dirs_meta( cat_path ):
-                for var_slug, var_path, var_meta in list_dirs_meta( entry_path ):
+        for cat_slug, cat_path in index_dirs( data_dir ):
+            for entry_slug, entry_path in index_dirs( cat_path ):
+                for var_slug, var_path in index_dirs( entry_path ):
                     yield {
                         'slug': var_slug,
                         'entry_slug': entry_slug,
                         'category_slug': cat_slug,
-                        'path': var_path,
-                        **cat_meta,
-                        **entry_meta,
-                        **var_meta }
+                        'path': var_path }
                 else:
                     yield {
                         'slug': entry_slug,
                         'category_slug': cat_slug,
-                        'path': entry_path,
-                        **cat_meta,
-                        **entry_meta }
+                        'path': entry_path }
     except ( FileNotFoundError, NotADirectoryError ) as e:
         print('Cannot proceed: %s' % e)
         sys.exit(1)
 
 test1 = """
-from __main__ import list_dirs_meta, is_var, index
-for i in index('data'):
-    try:
-        if is_var(i):
-            #print( i['entry_slug'] )
-            for n in index('data'):
-                #print( n )
-                if n['slug'] == i['entry_slug']:
-                    print( f"וואַריאַציע פֿון: {n['songname']}" )
-                #else: print( 'No match' )
-        print( f"{ i['var_name'] if ( 'var_name' in i.keys() ) else i['songname'] } — " 
-           f"{ i['linewidth'] }" )
-    except KeyError as e:
-        print( f'KeyError: { e } in { i["songname"] }' )
-    
-   
-    print( i['slug'] )
-"""
-
-test2 = """
-from __main__ import list_dirs_meta, is_var, index
+from __main__ import index_dirs, read_dir_meta, is_var, index
 list = []
 
 for i in index('data'):
@@ -97,11 +74,31 @@ for i in index('data'):
 for i in list:
     try:
         if is_var(i):
+            for n in list:
+                if n['slug'] == i['entry_slug']:
+                    print( f"Variation: {n['slug']}" )
+    except KeyError as e:
+        print( f'KeyError: { e } in { i["slug"] }' )
+    
+   
+    print( i['slug'] )
+"""
+
+test2 = """
+from __main__ import index_dirs, read_dir_meta, is_var, index
+list = []
+
+for i in index('data'):
+    list.append(i)
+for i in list:
+    i.update(**read_dir_meta(i['path']))
+    try:
+        if is_var(i):
             #print( i['entry_slug'] )
             for n in list:
                 #print( n )
                 if n['slug'] == i['entry_slug']:
-                    print( f"וואַריאַציע פֿון: {n['songname']}" )
+                    print( f"וואַריאַציע פֿון: {read_dir_meta(n['path'])['songname']}" )
                 #else: print( 'No match' )
         print( f"{ i['var_name'] if ( 'var_name' in i.keys() ) else i['songname'] } — " 
            f"{ i['linewidth'] }" )
@@ -113,57 +110,26 @@ for i in list:
 """
 
 test3 = """
-from __main__ import list_dirs_meta, is_var, index
-
-for n in index('data'):
-    if n['slug'] == 'rozhinkes-mit-mandlen':
-        print( n['songname'] )
-        break
-"""
-
-test4 = """
-from __main__ import list_dirs_meta, is_var, index
-list = []
-
-for i in index('data'):
-    list.append(i)
-
-for n in list:
-    if n['slug'] == 'rozhinkes-mit-mandlen':
-        print( n['songname'] )
-        break
-"""
-
-test5 = """
-from __main__ import list_dirs_meta, is_var, index
-
-songs = ['rozhinkes-mit-mandlen', 'afn-pripetchik']
-print([ n['songname'] for n in index('data') 
-    if n['slug'] in songs ] )
-
-"""
-
-test6 = """
-from __main__ import list_dirs_meta, is_var, index
-list = []
+from __main__ import index_dirs, read_dir_meta, is_var, index
 songs = ['rozhinkes-mit-mandlen', 'afn-pripetchik']
 
-for i in index('data'):
-    list.append(i)
+list = [ n for n in index( 'data' ) 
+    if n['slug'] in songs ]
 
-print([ n['songname'] for n in list 
-    if n['slug'] in songs ] )
+meta = [ (read_dir_meta(item['path'])['songname']) for item in list ]
+
+print(meta)
 """
 
+#index only
 time1 = timeit.timeit(test1, number=100)/100
+
+#index with meta
 time2 = timeit.timeit(test2, number=100)/100
-print(time1)
-print(time2) #faster
-#time3 = timeit.timeit(test3, number=100)/100
-#time4 = timeit.timeit(test4, number=100)/100
-#print(time3) #faster
-#print(time4)
-#time5 = timeit.timeit(test3, number=100)/100
-#time6 = timeit.timeit(test4, number=100)/100
-#print(time5) #faster
-#print(time6)
+
+#find songs from list and fetch meta
+time3 = timeit.timeit(test3, number=100)/100 
+
+print(time1) #0.0008s
+print(time2) #0.0262s
+print(time3) #0.0252s
