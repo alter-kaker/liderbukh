d5e8eafcffde62b87bd0df3282f6ca7ba4344081
Marc Trius <derpayatz@gmail.com>
2019-12-21 20:11:33 -0600

    Switched metadata to dict & made Tree objects iterable

--- liderbukh2.py
+++ liderbukh2.py
@@ -28,35 +28,35 @@ import timeit
 
 class Node:
     def __init__(self, slug, path, parent=None):
-        self.slug = slug
-        self.path = path
-        self.parent = parent
+        #print( f'{ slug }: Initializing...' )
+        self.meta = {
+            'slug': slug,
+            'path': path,
+            'parent': parent }
+        self.read_meta()
     
     def __repr__(self):
-        return self.slug
+        return self.meta['slug']
     
     def __call__(self, query=None):
         if not query:
             return self
         else:
-            if self.slug in query:
+            if self.meta['slug'] in query:
                 return [self]
             else:
                 return None
     
     def read_meta(self):
+        #print( f"{ self.meta['slug'] }: Reading meta file" )
         try:
             with open(
-                os.path.join(self.path, 'meta.yaml' ) ) as f:
-                meta = yaml.load( f )
-                
+                os.path.join(self.meta['path'], 'meta.yaml' ) ) as f:
+                self.meta.update( yaml.load( f ) )
         except Exception as e:
              print(
-                 f'Cannot open meta file for { self.slug }: { e }'
+                 f"{ self.meta['slug'] }: Cannot open meta file: { e }"
                  )
-        
-        for key, val in meta.items():
-            setattr( self, key, val )
 
 class Tree(Node):
     def __init__(self, slug, path, parent=None):
@@ -68,9 +68,9 @@ class Tree(Node):
         for entry in self.index:
             contents.append(entry)
         if contents:
-            repre = '%s: %s' % (self.slug, contents)
+            repre = '%s: %s' % (self.meta['slug'], contents)
         else:
-            repre = self.slug
+            repre = self.meta['slug']
         return repre
     
     def __call__(self, query=False):
@@ -81,7 +81,7 @@ class Tree(Node):
             except AttributeError:
                 query = [query]
             for entry in self.index:
-                if entry.slug in query:
+                if entry.meta['slug'] in query:
                     result.append( entry )
                 else:
                     try:
@@ -89,27 +89,30 @@ class Tree(Node):
                     except:
                         pass
             return result
-        else:
-            return self.index
+    
+    def __iter__(self):
+        return self.index.__iter__()
+    
+    def __getitem__(self, key):
+        return self.index[key]
     
     def generate_index(self):
+        #print( f'{self.meta['slug']}: Scanning folder...' )
         try:
             for dir_path in (
                     directory.path
-                    for directory in os.scandir( self.path ) 
+                    for directory in os.scandir( self.meta['path'] ) 
                     if directory.is_dir()
                     and os.path.isfile(
                         os.path.join( directory.path, 'meta.yaml' ) ) ):
                 slug, path = os.path.split(dir_path)[1], dir_path
                 yield self.child(slug, path, self)
         except Exception as e:
-            print(f'Exception: {e}')        
+            print(f"{ self.meta['slug'] }: Cannot generate index: {e}")
+            raise
 
 class Entry(Tree):
     child = Node
-    def __init__(self, slug, path, parent=None):
-        super().__init__(slug, path, parent)
-        self.index.insert(0, self.child(slug, path, self))
 
 class Category(Tree):
     child = Entry
@@ -119,25 +122,26 @@ class Book(Tree):
 
 test1 = """
 from __main__ import Node, Tree, Category, Entry, Book
+print('test1')
 tree = Book('data', 'data')
 
-#print(tree)
 """
 
 test2 = """
 from __main__ import Node, Tree, Category, Entry,  Book
+print('test2')
 tree = Book('data', 'data')
-result = [n for n in tree(['rozhinkes-mit-mandlen', 'afn-pripetchik'])]
+result = tree(['rozhinkes-mit-mandlen', 'afn-pripetchik'])
 
 #print(result)
 """
 
 
-##build tree using Entry
-#time1 = timeit.timeit(test1, number=1000)
+#build tree using Entry
+time1 = timeit.timeit(test1, number=10)
 
-##build and query tree using Entry
-#time2 = timeit.timeit(test2, number=1000)
+#build and query tree using Entry
+time2 = timeit.timeit(test2, number=10)
 
-#print(time1)
-#print(time2)
+print(time1)
+print(time2)
